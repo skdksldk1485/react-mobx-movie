e5dc54d78aba9a40af7e2fea8ad74414d76cfce2
skdksldk1485 <skdksldk1485@naver.com>
2021-05-30 02:23:02 +0900

    컴포넌트 Credits 디자인 수정

--- src/component/Credits.js
+++ src/component/Credits.js
@@ -8,11 +8,11 @@ const Credits = (props) => {
     const castSlice = store.cast.slice(0, store.castCount);
     const cast = castSlice.map(castList => {
       return (
-        <div className="Credit__List" key={castList.id}>
-          <div className="Profile__Photo">
+        <div className="Credit__List__Wrap" key={castList.id}>
+          <div className="Credit__List__Wrap__Profile">
           {castList.profile_path == null ? <i class="fas fa-user"></i> : <img src={'https://image.tmdb.org/t/p/w300'+ castList.profile_path} alt={ castList.name }/>}
           </div>
-          <p className="Top"><span>{castList.character}</span>역</p><p className="Bottom">{castList.name}</p>
+          <p className="Credit__List__Wrap__Top"><span>{castList.character}</span>역</p><p className="Bottom">{castList.name}</p>
         </div>
       )
     });
@@ -27,18 +27,18 @@ const Credits = (props) => {
   const posterUrl = 'https://image.tmdb.org/t/p/w300';
 
   return useObserver(() => (
-    <div className="Credits">
+    <div className="Credit">
       <h3>이 영화의 감독 / 출연진</h3>
-      <div className="Credit__List__Wrap">
-        <div className="Credit__List">
-          <div className="Profile__Photo">
+      <div className="Credit__List">
+        <div className="Credit__List__Wrap">
+          <div className="Credit__List__Wrap__Profile">
           {store.director.path == null ? <i class="fas fa-user"></i> : <img src={posterUrl + store.director.path} alt={store.director.name} />}
           </div>
-          <p className="Top">감독</p><p className="Bottom">{store.director.name}</p>
+          <p className="Credit__List__Wrap__Top">감독</p><p className="Bottom">{store.director.name}</p>
         </div>
         {renderCast()}
       </div>
-      <div className="More" onClick={handleCastCount}><i className="fas fa-caret-down"></i>더 보기</div>
+      <div className="Credit__More" onClick={handleCastCount}><i className="fas fa-caret-down"></i>더 보기</div>
     </div>
   ));
 }
